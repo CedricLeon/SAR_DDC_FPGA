e3ad2f2370f2fc78c459f99f6c041d39743a48df
CedricLeon <leonard99cedric@gmail.com>
2026-02-20 15:43:13 +0100

    feat(results): Saved most recent training schdule

--- scripts/schedule_training.sh
+++ scripts/schedule_training.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
 
-# Schedule execution of many training runs
 # Run from root folder with: bash scripts/schedule_training.sh
-python src/train.py -m experiment=ADAM seed=3 model.criterion.lmbda=1.5,2,5,10,20,50,100,200,500,1000 model.net.no_output_padding=True,False
 
+# ----- Short schedule for training missing runs -----
+python src/train.py -m experiment=ADAM seed=5 model.criterion.lmbda=1,2,5,10,20,50,100,200,500,1000 model.net.activation="relu" model.net.no_output_padding=False
+python src/train.py -m experiment=ADAM seed=5 model.criterion.lmbda=1,2,5,10,20,50,100,200,500,1000 model.net.activation="gdn" model.net.no_output_padding=True,False
 
-# # ---- with MERLIN normalization -----
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-21_12-42-02/0/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-21_12-42-02/1/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-21_12-42-02/2/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-21_12-42-02/3/checkpoints/last.ckpt
+# # ----- Full schedule of results -----
+# # Generate Ground-truth checkpoints
+# python src/train.py experiment=MERLIN
+# python src/train.py experiment=ADAM-NOC
 
-# # ---- with 0 (min/max) normalization -----
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/0/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/1/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/2/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/3/checkpoints/last.ckpt
-# python src/eval.py ckpt_path=logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/4/checkpoints/last.ckpt
+# # Create datasets
+# # @TODO: Get there paths to feed to create_dataset.py
+# python scripts/create_dataset.py
 
-# # Create a new folder and copy all "inference_results_..." folders from each checkpoint directory in it
-# mkdir tmp_to_download
-# for i in {0..4}
-# do
-#  echo $i
-#  cp -r logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/$i/inference_results_* tmp_to_download/
-#  cp -r logs/train/sar_ddc/hyperprior/multiruns/2025-06-21_12-42-02/$i/inference_results_* tmp_to_download/
-# done
-# cp -r logs/train/sar_ddc/hyperprior/multiruns/2025-06-20_18-34-17/4/inference_results_* tmp_to_download/
+# # Full training sweep ~13days
+# python src/train.py -m experiment=ADAM seed=0,1,2,3,4 model.criterion.lmbda=1,2,5,10,20,50,100,200,500,1000 model.net.no_output_padding=True,False model.net.activation="gdn","relu"
 
-# # Then for each folder in tmp_to_download, copy directly in tmp_to_download the files starting with "output_reflectivity_...", "output_real_...", "input_reflectivity_..."
-# for folder in tmp_to_download/inference_results_*
-# do
-#   echo "Processing $folder"
-#   cp "$folder"/output_reflectivity_*.png tmp_to_download/
-#   cp "$folder"/output_real_*.png tmp_to_download/
-#   cp "$folder"/input_reflectivity_*.png tmp_to_download/
-# done
-# # zip the whole tmp_to_download folder
-# zip -r tmp_to_download.zip tmp_to_download
+# # Deployment on FPGA and evaluation
+# # @TODO
+# python master_deploy.py
